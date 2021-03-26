8ba413a9e4d2f48dad0baa82394afad2ee2e7883
John Russo <russojd@.ohsu.edu>
2021-03-26 10:43:33 -0700

    Added caching workflow

---
+++ .github/workflows/setup_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+conda init bash
+
+conda env create -f environment.yml
+
+# Install sphinx
+sudo apt update -y
+sudo apt install python3-sphinx
