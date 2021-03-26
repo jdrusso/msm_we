a40bdea79a5099cf267dcdb181540cf2d2ff0b1e
John Russo <russojd@.ohsu.edu>
2021-03-26 11:19:00 -0700

    Fixes to miniconda in workflow

--- .github/workflows/setup_environment.sh
+++ .github/workflows/setup_environment.sh
@@ -2,12 +2,15 @@
 
 # Install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 
 conda init bash
+source ~/.bashrc
 
 conda env create -f environment.yml
 
 # Install sphinx
-sudo apt update -y
-sudo apt install python3-sphinx
+sudo apt-get update -y
+sudo apt-get install python3-sphinx -y
+
+pip install sphinx_rtd_theme
