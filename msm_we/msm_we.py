cc31d4b93f5ad7a1401dfb5a95b0b7b8d4332836
John Russo <russojd@.ohsu.edu>
2021-07-14 14:27:48 -0700

    Changed processCoordinates and reduceCoordinates to be mandatory user-overridden

--- msm_we/msm_we.py
+++ msm_we/msm_we.py
@@ -1317,81 +1317,72 @@ class modelWE:
         def transform(self, coords):
             return coords
 
-    # TODO: Maybe this should be user-overrideable, it's up to them to calculate a reduced coordinate to cluster on
-    def processCoordinates(self, coords):
-        log.debug("Processing coordinates")
-
-        if self.dimReduceMethod == "none":
-            nC = np.shape(coords)
-            nC = nC[0]
-            ndim = 3 * self.nAtoms
-            data = coords.reshape(nC, 3 * self.nAtoms)
-            return data
-
-        if self.dimReduceMethod == "pca" or self.dimReduceMethod == "vamp":
-
-            ### Original dimensionality reduction
-            xt = md.Trajectory(xyz=coords, topology=None)
-            atom_selection_string = "resid 1"
-            # indCA = self.reference_structure.topology.select("name CA")
-            # pair1, pair2 = np.meshgrid(indCA, indCA, indexing="xy")
-            # indUT = np.where(np.triu(pair1, k=1) > 0)
-            # pairs = np.transpose(np.array([pair1[indUT], pair2[indUT]])).astype(int)
-            # dist = md.compute_distances(xt, pairs, periodic=True, opt=True)
-
-            ###
-
-            ### NaCl dimensionality reduction
-            log.warning("Hardcoded selection: Doing dim reduction for Na, Cl. This is only for testing!")
-            indNA = self.reference_structure.topology.select("element Na")
-            indCL = self.reference_structure.topology.select("element Cl")
-
-            diff = np.subtract(coords[:, indNA], coords[:, indCL])
-
-            dist = np.array(np.sqrt(
-                np.mean(
-                    np.power(
-                        diff,
-                        2)
-                , axis=-1)
-            ))
-
-            ###
-
-            # assert not dist.shape[-1] == 0
-
-            return dist
-
-    def reduceCoordinates(self, coords):
+    # def processCoordinates(self, coords):
+    def processCoordinates(self):
         """
-        TODO: Make this user-overrideable
-
-        Defines the coordinate reduction strategy used.
-        The reduced corodinates are stored in /auxdata for each iteration.
-
-        Parameters
-        ----------
-        coords: array-like
-            Array of coordinates to reduce.
-
-        Returns
-        -------
-        Reduced data
-
+        User-overrideable function to process coordinates.
         """
 
-        log.debug("Reducing coordinates")
+        raise NotImplementedError
+        # log.debug("Processing coordinates")
+        #
+        # if self.dimReduceMethod == "none":
+        #     nC = np.shape(coords)
+        #     nC = nC[0]
+        #     ndim = 3 * self.nAtoms
+        #     data = coords.reshape(nC, 3 * self.nAtoms)
+        #     return data
+        #
+        # if self.dimReduceMethod == "pca" or self.dimReduceMethod == "vamp":
+        #
+        #     ### Original dimensionality reduction
+        #     xt = md.Trajectory(xyz=coords, topology=None)
+        #     atom_selection_string = "resid 1"
+        #     # indCA = self.reference_structure.topology.select("name CA")
+        #     # pair1, pair2 = np.meshgrid(indCA, indCA, indexing="xy")
+        #     # indUT = np.where(np.triu(pair1, k=1) > 0)
+        #     # pairs = np.transpose(np.array([pair1[indUT], pair2[indUT]])).astype(int)
+        #     # dist = md.compute_distances(xt, pairs, periodic=True, opt=True)
+        #
+        #     ###
+        #
+        #     ### NaCl dimensionality reduction
+        #     log.warning("Hardcoded selection: Doing dim reduction for Na, Cl. This is only for testing!")
+        #     indNA = self.reference_structure.topology.select("element Na")
+        #     indCL = self.reference_structure.topology.select("element Cl")
+        #
+        #     diff = np.subtract(coords[:, indNA], coords[:, indCL])
+        #
+        #     dist = np.array(np.sqrt(
+        #         np.mean(
+        #             np.power(
+        #                 diff,
+        #                 2)
+        #         , axis=-1)
+        #     ))
+        #
+        #     return dist
 
-        if self.dimReduceMethod == "none":
-            nC = np.shape(coords)
-            nC = nC[0]
-            ndim = 3 * self.nAtoms
-            data = coords.reshape(nC, 3 * self.nAtoms)
-            return data
-        if self.dimReduceMethod == "pca" or self.dimReduceMethod == "vamp":
-            coords = self.processCoordinates(coords)
-            coords = self.coordinates.transform(coords)
-            return coords
+    # def reduceCoordinates(self, coords):
+    def reduceCoordinates(self):
+        """
+        User-overrideable function to reduce coordinates.
+        """
+
+        raise NotImplementedError
+
+        # log.debug("Reducing coordinates")
+        #
+        # if self.dimReduceMethod == "none":
+        #     nC = np.shape(coords)
+        #     nC = nC[0]
+        #     ndim = 3 * self.nAtoms
+        #     data = coords.reshape(nC, 3 * self.nAtoms)
+        #     return data
+        # if self.dimReduceMethod == "pca" or self.dimReduceMethod == "vamp":
+        #     coords = self.processCoordinates(coords)
+        #     coords = self.coordinates.transform(coords)
+        #     return coords
 
     def update_cluster_structures(self):
         """
