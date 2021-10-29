8ac63c689cf92bdca70f984946dc0062401c4ce8
John Russo <russojd@.ohsu.edu>
2021-10-29 14:44:57 -0700

    Updated dependencies and CI

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["matplotlib", "networkx", "scipy"]
+# Ray must be installed through pip, not conda
+requirements = ["ray"]
 
 setup_requirements = [
     "pytest-runner",
