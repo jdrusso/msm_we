a6365cef1902c1d293dfb12cde6c94dde1de8515
John Russo <russojd@.ohsu.edu>
2021-12-07 17:11:57 -0800

    Update WESTPA dependency to dev version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 # WESTPA-2.0 with westpa.analysis is not yet in Conda
-requirements = ["westpa>=v2.0b5"]
+requirements = ["westpa>=v2.0.dev1"]
 
 setup_requirements = [
     "pytest-runner",
