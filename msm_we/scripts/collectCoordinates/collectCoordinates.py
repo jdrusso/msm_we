8448a17eb4a93f76ccd487f6089a1d49489cd13d
JD Russo <russojd@ohsu.edu>
2022-05-05 12:44:55 -0700

    Hotfix to collectCoordinates.py

--- msm_we/scripts/collectCoordinates/collectCoordinates.py
+++ msm_we/scripts/collectCoordinates/collectCoordinates.py
@@ -1,19 +1,15 @@
-import matplotlib
-
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-# sys.path.append("/home/groups/copperma/ZuckermanLab/msmWE")
-# import msm_we
 import h5py
 import pickle
 import os
 import subprocess
 import mdtraj as md
 
-sys.path.append("/Users/russojd/Research/molssi_2021/msmWE_cc/msm_we")
+import logging
+log = logging.getLogger()
+
 from msm_we import msm_we
 """
 This code
@@ -25,64 +21,62 @@ This code
 3. Reads through each of the trajectory files, putting the coordinates into the H5 file's auxdata/coord (if they don't already exist)
 """
 
-# fileSpecifier='/home/groups/ZuckermanLab/mostofia/ProteinFolding/NTL9/WestPA/Gamma05ps/Pcoord1D/OpenMM/Run03/west_prev.h5'
-# refPDBfile='/home/groups/ZuckermanLab/copperma/msmWE/NTL9/reference.pdb'
-# initPDBfile='/home/groups/ZuckermanLab/copperma/msmWE/NTL9/coor0.pdb'
-# modelName='NTL9_Run03_jan9'
-fileSpecifier = sys.argv[1]
+we_h5filename = sys.argv[1]
 refPDBfile = sys.argv[2]
-initPDBfile = sys.argv[3]
-modelName = sys.argv[4]
-WEfolder = sys.argv[5]
+modelName = sys.argv[3]
+WEfolder = sys.argv[4]
+pcoord_ndim = sys.argv[5]
 
-# Model is loaded to get the number of iterations and segments
 model = msm_we.modelWE()
-print("initializing...")
-model.initialize(fileSpecifier, refPDBfile, initPDBfile, modelName)
+log.info('Preparing coordinates...')
+
+# Only need the model to get the number of iterations and atoms
+# TODO: Replace this with something more lightweight, get directly from WE
+log.debug(f'Doing collectCoordinates on  WE file {we_h5filename}')
+model.initialize(
+    [we_h5filename],
+    refPDBfile,
+    modelName,
+    # Pass some dummy arguments -- these aren't important, this model is just created for convenience
+    # in the coordinate collection. Dummy arguments prevent warnings from being raised.
+    basis_pcoord_bounds=None,
+    target_pcoord_bounds=None,
+    tau=1,
+    pcoord_ndim=pcoord_ndim,
+    _suppress_boundary_warning=True,
+)
 model.get_iterations()
 
-f = h5py.File(model.fileList[0], "a")
-
-# For each iteration, look at all the trajectory segments that were generated and add them to the h5
-
-# Loop over iterations
-for n_iter in range(1, model.maxIter + 1):
-
-    # Print some logging output every 10 steps
-    if n_iter % 10 == 0:
-        sys.stdout.write("copying coords into westfile iteration " + str(n_iter) + "\n")
+n_iter = None
+for n_iter in tqdm.tqdm(range(1, model.maxIter + 1)):
 
     nS = model.numSegments[n_iter - 1].astype(int)
-
-    # Coords will be an array indexable by
-    #   (Segment, Parent/Child, Atom, Position)
     coords = np.zeros((nS, 2, model.nAtoms, 3))
+    dsetName = "/iterations/iter_%08d/auxdata/coord" % int(n_iter)
 
-    # Loop over all segments in this iteration
+    coords_exist = False
+    try:
+        dset = h5file.create_dataset(dsetName, np.shape(coords))
+    except (RuntimeError, ValueError):
+        log.debug('coords exist for iteration ' + str(n_iter) + ' NOT overwritten')
+        coords_exist = True
+        continue
+
     for iS in range(nS):
-
-        # Load up the file corresponding to this segment, and extract the relevant coordinates
         trajpath = WEfolder + "/traj_segs/%06d/%06d" % (n_iter, iS)
 
-        parent_coords = np.squeeze(
-            md.load(
-                trajpath + "/parent.xml", top=model.reference_structure.topology
-            )._xyz
-        )
-        child_coords = np.squeeze(
-            md.load(trajpath + "/seg.xml", top=model.reference_structure.topology)._xyz
-        )
-        coords[iS, 0, :, :] = parent_coords
-        coords[iS, 1, :, :] = child_coords
+        try:
+            coord0 = np.squeeze(md.load(f'{trajpath}/{parentTraj}', top=model.reference_structure.topology)._xyz)
+        except OSError:
+            log.warning("Parent traj file doesn't exist, loading reference structure coords")
+            coord0 = np.squeeze(model.reference_structure._xyz)
 
-    # Try to put the newly-obtained coordinates in the H5 file, if they're not already there.
-    dsetName = "/iterations/iter_%08d/auxdata/coord" % int(n_iter)
-    try:
-        dset = f.create_dataset(dsetName, np.shape(coords))
+        coord1 = np.squeeze(md.load(f'{trajpath}/{childTraj}', top=model.reference_structure.topology)._xyz)
+
+        coords[iS, 0, :, :] = coord0
+        coords[iS, 1, :, :] = coord1
+
+    if not coords_exist:
         dset[:] = coords
-    except:
-        sys.stdout.write(
-            "coords exist for iteration " + str(n_iter) + " NOT overwritten\n"
-        )
 
-f.close()
+log.debug(f"Wrote coords for {n_iter} iterations.")
