611a2fa3c9bd5a6409361c00e0e3244a2c7353e3
John Russo <jdr033@bucknell.edu>
2022-08-04 17:57:28 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     tests_require=test_requirements,
     extras_require=EXTRAS_REQUIRE,
     url="https://github.com/jdrusso/msm_we",
-    version="0.1.13",
+    version="0.1.14",
     zip_safe=False,
 )
