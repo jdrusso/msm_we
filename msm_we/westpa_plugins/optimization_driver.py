68005060403d4fdab384f01bdf68d8fe8ab9c3a8
John Russo <jdrusso@users.noreply.github.com>
2022-10-05 15:30:39 -0700

    Fix pcoord extension

--- msm_we/westpa_plugins/optimization_driver.py
+++ msm_we/westpa_plugins/optimization_driver.py
@@ -8,17 +8,25 @@ import numpy as np
 import pickle
 from rich.progress import Progress
 import ray
+from synd.westpa.propagator import get_segment_parent_index
+from westpa.core.data_manager import create_dataset_from_dsopts
 
 
 @ray.remote
 class GlobalModelActor:
-    def __init__(self, model, processCoordinates):
+    def __init__(self, model, processCoordinates, synd_model, original_pcoord_ndim):
         msm_we.msm_we.modelWE.processCoordinates = processCoordinates
         self.model = model
-        assert hasattr(self.model, 'processCoordinates')
+        assert hasattr(self.model, "processCoordinates")
+
+        self.backmap = synd_model.backmap
+        self.original_pcoord_ndim = original_pcoord_ndim
 
     def get_model(self):
         return self.model
+
+    def get_original_pcoord(self, state_index):
+        return self.backmap(state_index)[:self.original_pcoord_ndim]
 
 
 @ray.remote
@@ -31,9 +39,18 @@ class PcoordCalculator:
     def compute_new_structure_pcoord(self, structure, state_index):
         reduceCoordinates = self.model.reduceCoordinates
 
-        reduced_coords = reduceCoordinates(structure)
+        # Take the zero index, because we're just passing a single structure
+        reduced_coords = reduceCoordinates(structure)[0]
 
-        return reduced_coords, state_index
+        original_pcoord = ray.get(
+            self.model_actor.get_original_pcoord.remote(
+                state_index
+            )
+        )
+
+        new_pcoord = np.concatenate([original_pcoord, reduced_coords])
+
+        return new_pcoord, state_index
 
 
 class OptimizationDriver:
@@ -54,17 +71,26 @@ class OptimizationDriver:
         self.data_manager = sim_manager.data_manager
         self.sim_manager = sim_manager
         self.we_driver = westpa.rc.get_we_driver()
+        self.propagator = westpa.rc.get_propagator()
+
+        self.synd_model = westpa.rc.get_propagator().synd_model
 
         self.plugin_config = plugin_config
 
-        coord_map_path = plugin_config.get('full_coord_map')
-        with open(coord_map_path, 'rb') as infile:
+        coord_map_path = plugin_config.get("full_coord_map")
+        with open(coord_map_path, "rb") as infile:
             self.coord_map = pickle.load(infile)
 
         # Big number is low priority -- this should run before anything else
-        self.priority = plugin_config.get('priority', 3)
+        self.priority = plugin_config.get("priority", 3)
 
-        sim_manager.register_callback(sim_manager.finalize_run, self.do_optimization, self.priority)
+        self.original_pcoord_dim = westpa.rc.config.get(
+            ["west", "system", "system_options", "pcoord_ndim"]
+        )
+
+        sim_manager.register_callback(
+            sim_manager.finalize_run, self.do_optimization, self.priority
+        )
 
     def do_optimization(self):
         """
@@ -89,18 +115,25 @@ class OptimizationDriver:
         #   whatever it may be, and then additionally the result of `model.reduceCoordinates` on the full-coord
         #   structure (not sure the best way to get that, it is eventually stored in auxdata)
         westpa.rc.pstatus("Updating pcoord map")
-        propagator = westpa.rc.get_propagator()
+        # propagator = self.propagator
         new_pcoord_map = self.compute_new_pcoord_map()
-        propagator.pcoord_map = new_pcoord_map
+        self.update_pcoord_map(new_pcoord_map)
 
         # 4. Continue WE, with optimized parameters
         # No need to re-initialize/restart, just extend max iterations and continue
-        remaining_iters = self.plugin_config.get('max_iters') - self.sim_manager.max_total_iterations
+        remaining_iters = (
+            self.plugin_config.get("max_iters") - self.sim_manager.max_total_iterations
+        )
         if remaining_iters > 0:
-            new_iters = min(remaining_iters, westpa.rc.config.get(['west', 'propagation', 'max_total_iterations']))
+            new_iters = min(
+                remaining_iters,
+                westpa.rc.config.get(["west", "propagation", "max_total_iterations"]),
+            )
             self.sim_manager.max_total_iterations += new_iters
 
-            westpa.rc.pstatus(f"\n\n=== Applying optimization and continuing for {new_iters} more iterations ===\n")
+            westpa.rc.pstatus(
+                f"\n\n=== Applying optimization and continuing for {new_iters} more iterations ===\n"
+            )
             w_run.run_simulation()
 
         else:
@@ -116,7 +149,7 @@ class OptimizationDriver:
         new walker allocation over the WE bins.
         """
 
-        allocation_strategy = self.plugin_config.get('allocation_strategy', None)
+        allocation_strategy = self.plugin_config.get("allocation_strategy", None)
 
         if allocation_strategy is None:
             westpa.rc.pstatus("\tNot updating allocation")
@@ -143,25 +176,23 @@ class OptimizationDriver:
         """
 
         model = self.data_manager.hamsm_model
-        binning_strategy = self.plugin_config.get('binning_strategy', None)
+        binning_strategy = self.plugin_config.get("binning_strategy", None)
 
         n_active_bins = np.count_nonzero(self.we_driver.bin_target_counts)
 
         if binning_strategy is None:
 
-            westpa.rc.pstatus("\tUsing k-means MFPT optimization (optimization.get_clustered_mfpt_bins) "
-                              "for bin optimization")
+            westpa.rc.pstatus(
+                "\tUsing k-means MFPT optimization (optimization.get_clustered_mfpt_bins) "
+                "for bin optimization"
+            )
 
             discrepancy, variance = optimization.solve_discrepancy(
-                tmatrix=model.Tmatrix,
-                pi=model.pSS,
-                B=model.indTargets
+                tmatrix=model.Tmatrix, pi=model.pSS, B=model.indTargets
             )
 
             microstate_assignments = optimization.get_clustered_mfpt_bins(
-                variance, discrepancy,
-                model.pSS,
-                n_active_bins
+                variance, discrepancy, model.pSS, n_active_bins
             )
 
         else:
@@ -178,7 +209,8 @@ class OptimizationDriver:
 
         # 3. Update binning
         base_mapper = model.clusters.bin_mapper
-        n_pcoord_dims = 1
+
+        n_pcoord_dims = self.original_pcoord_dim
 
         we_bin_mapper = optimization.OptimizedBinMapper(
             n_active_bins,
@@ -191,7 +223,7 @@ class OptimizationDriver:
             #   Used for stratified clustering
             base_mapper,
             microstate_assignments,
-            model.clusters
+            model.clusters,
         )
 
         return we_bin_mapper
@@ -210,33 +242,40 @@ class OptimizationDriver:
 
         new_pcoord_map = {}
 
-        n_actors = int(ray.available_resources().get('CPU', 1))
-        model_actor = GlobalModelActor.remote(model, processCoordinates)
-        pcoord_calculators = [PcoordCalculator.remote(model_actor, processCoordinates) for i in range(n_actors)]
+        n_actors = int(ray.available_resources().get("CPU", 1))
+        model_actor = GlobalModelActor.remote(
+            model, processCoordinates, self.synd_model, self.original_pcoord_dim
+        )
+        pcoord_calculators = [
+            PcoordCalculator.remote(
+                model_actor, processCoordinates
+            )
+            for i in range(n_actors)
+        ]
 
         msm_we.msm_we.modelWE.check_connect_ray()
         ids = []
 
         with Progress() as progress:
-            submit_task = progress.add_task("Submitting structures for pcoord calculation",
-                                            total=len(self.coord_map))
-            retrieve_task = progress.add_task(f"Retrieving structure pcoords from {n_actors} workers",
-                                              total=len(self.coord_map))
+            submit_task = progress.add_task(
+                "Submitting structures for pcoord calculation",
+                total=len(self.coord_map),
+            )
+            retrieve_task = progress.add_task(
+                f"Retrieving structure pcoords from {n_actors} workers",
+                total=len(self.coord_map),
+            )
 
             for state_index, structure in self.coord_map.items():
                 _id = pcoord_calculators[
-                        state_index % n_actors
-                    ].compute_new_structure_pcoord.remote(
-                        structure, state_index
-                    )
+                    state_index % n_actors
+                ].compute_new_structure_pcoord.remote(structure, state_index)
                 ids.append(_id)
 
                 progress.advance(submit_task)
 
             while ids:
-                finished, ids = ray.wait(
-                    ids, num_returns=min(50, len(ids)), timeout=5
-                )
+                finished, ids = ray.wait(ids, num_returns=min(50, len(ids)), timeout=5)
                 results = ray.get(finished)
 
                 for pcoord, state_index in results:
@@ -244,3 +283,113 @@ class OptimizationDriver:
                     progress.advance(retrieve_task)
 
         return new_pcoord_map
+
+    def update_pcoord_map(self, new_pcoord_map):
+        # TODO: Janky
+
+        self.propagator.synd_model._backmapper = new_pcoord_map.get
+        new_pcoord_dim = new_pcoord_map.get(0).shape[0]
+        westpa.rc.pstatus(f"New pcoord dimensionality is {new_pcoord_dim}")
+
+        system = westpa.rc.get_system_driver()
+        data_manager = westpa.rc.get_data_manager()
+        sim_manager = westpa.rc.get_sim_manager()
+
+        # This function causes problems when extending pcoords, and isn't actually used for anything, so just skip it
+        data_manager.get_new_weight_data = lambda x: None
+
+        #  # Update system driver
+        system.pcoord_ndim = new_pcoord_dim
+
+        #  # Update the pcoord dataset in west.h5
+        data_manager.open_backing()
+
+        iter_group = data_manager.get_iter_group(sim_manager.n_iter)
+        n_iter = sim_manager.n_iter
+        segments = data_manager.get_segments(
+            n_iter=sim_manager.n_iter, load_pcoords=True
+        )
+
+        del data_manager.we_h5file[
+            "/iterations/iter_{:0{prec}d}/pcoord".format(
+                int(n_iter), prec=data_manager.iter_prec
+            )
+        ]
+        data_manager.flush_backing()
+
+        pcoord_opts = data_manager.dataset_options.get(
+            "pcoord",
+            {"name": "pcoord", "h5path": "pcoord", "compression": False},
+        )
+
+        #  # Update the currently held segments
+        westpa.rc.pstatus(f"Attempting to fetch segments for iter {sim_manager.n_iter}")
+        for segment in segments:
+
+            parent_state_index = get_segment_parent_index(
+                segment
+            )
+
+            segment.pcoord = np.concatenate(
+                [
+                    [new_pcoord_map[parent_state_index]],
+                    np.zeros(
+                        shape=(system.pcoord_len - 1, system.pcoord_ndim)
+                    ),
+                ]
+            )
+
+        create_dataset_from_dsopts(
+            iter_group,
+            pcoord_opts,
+            data=np.array([segment.pcoord for segment in segments]),
+            shape=(
+                len(sim_manager.segments),
+                system.pcoord_len,
+                system.pcoord_ndim,
+            ),
+            dtype=system.pcoord_dtype,
+        )
+
+        data_manager.update_segments(sim_manager.n_iter, segments)
+
+        # The initial states that were computed for the next iteration are using the old-style pcoord,
+        #   so we can't bin them correctly with the OptimizedBinMapper unless we update them.
+        # Although the old istates won't be used if you have gen_istate enabled, we still have to
+        #   recreate the dataset. Otherwise, it'll be the wrong shape for the new istates.
+        initial_states = data_manager.get_initial_states(sim_manager.n_iter)
+        n_initial_states = len(initial_states)
+
+        del data_manager.we_h5file[
+            "/iterations/iter_{:0{prec}d}/ibstates/istate_pcoord".format(
+                int(n_iter), prec=data_manager.iter_prec
+            )
+        ]
+        del data_manager.we_h5file[
+            "/iterations/iter_{:0{prec}d}/ibstates/istate_index".format(
+                int(n_iter), prec=data_manager.iter_prec
+            )
+        ]
+
+        new_istates = data_manager.create_initial_states(
+            n_states=n_initial_states, n_iter=n_iter
+        )
+
+        for old_istate, new_istate in zip(initial_states, new_istates):
+            new_istate = old_istate
+            bstate_id = old_istate.basis_state_id
+            parent_state_index = int(
+                sim_manager.next_iter_bstates[bstate_id].auxref
+            )
+            new_istate.pcoord = new_pcoord_map[parent_state_index]
+
+        data_manager.flush_backing()
+
+        for segment in sim_manager.segments.values():
+            parent_state = get_segment_parent_index(
+                segment
+            )
+            segment.pcoord = new_pcoord_map[parent_state]
+
+        data_manager.flush_backing()
+        data_manager.close_backing()
