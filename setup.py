5689dfcee9701a74129b2ad311d53b076439af48
John Russo <jdrusso@users.noreply.github.com>
2022-10-11 14:07:39 -0700

    Updated version to 0.1.27

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     tests_require=test_requirements,
     extras_require=EXTRAS_REQUIRE,
     url="https://github.com/jdrusso/msm_we",
-    version="0.1.25",
+    version="0.1.27",
     zip_safe=False,
 )
