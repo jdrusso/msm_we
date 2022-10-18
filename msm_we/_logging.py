1099be01536640edd825e556d3d15110299348f2
John Russo <jdrusso@users.noreply.github.com>
2022-10-17 22:11:58 -0700

    Use DefaultProgress class to instantiate progress bars

--- msm_we/_logging.py
+++ msm_we/_logging.py
@@ -2,7 +2,7 @@
 
 import logging
 from rich.logging import RichHandler
-from rich.progress import Progress
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -10,12 +10,21 @@ log.addHandler(RichHandler())
 log.propagate = False
 
 
+class DefaultProgress(Progress):
+
+    def __init__(self, args=[], **kwargs):
+
+        if not args:
+            args = [SpinnerColumn(), *Progress.get_default_columns(), TimeElapsedColumn()]
+
+        super().__init__(*args, **kwargs)
+
 class ProgressBar:
 
     def __init__(self, progress_bar: Progress = None):
 
         if progress_bar is None:
-            self._progress_bar = Progress()
+            self._progress_bar = DefaultProgress()
             self._used_existing = False
         else:
             self._progress_bar = progress_bar
