2ba06e39174bf587a0c763e129303037bc17cbf3
John Russo <jdrusso@users.noreply.github.com>
2022-10-17 22:02:23 -0700

    Bugfix

--- msm_we/_hamsm/_clustering.py
+++ msm_we/_hamsm/_clustering.py
@@ -736,7 +736,7 @@ class ClusteringMixin:
 
         self.clusters.toggle = False
 
-        self.launch_ray_discretization()
+        self.launch_ray_discretization(progress_bar)
 
     def do_stratified_clustering(self: "modelWE", arg):
         """
