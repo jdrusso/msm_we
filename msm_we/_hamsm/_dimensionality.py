863908ecbcfc6e1711f128aa532e6a14d8f63c49
JD Russo <1316917+jdrusso@users.noreply.github.com>
2022-10-28 12:26:15 -0700

    Rich-style progress bars, some improvements to dimensionality reduction and plugin config (#34)
    * Bugfix to clustering
    
    * Fix progress bars in validation models
    
    * Added pre-commit for lint+black
    
    Sanity-check on number of WE bins to optimization
    
    * Added progress bar for block validation
    
    * Hide job submission progress bars when complete
    
    * Allow specifying use weights in dimensionality reduction in plugin config
    
    * Added support for batch-PCA
    
    * Bugfix to pcoord update -- wasn't updating the correct backmapper
    
    * Allow specifying number of validation groups to hamsm plugin
    
    * Add deeptime requirement
    
    * Bugfix to clustering
    
    * Fix progress bars in validation models
    
    * Added pre-commit for lint+black
    
    Sanity-check on number of WE bins to optimization
    
    * Added progress bar for block validation
    
    * Hide job submission progress bars when complete
    
    * Allow specifying use weights in dimensionality reduction in plugin config
    
    * Added support for batch-PCA
    
    * Bugfix to pcoord update -- wasn't updating the correct backmapper
    
    * Allow specifying number of validation groups to hamsm plugin
    
    * Add deeptime requirement
    
    * Added tests for optimization (#31)
    
    Co-authored-by: John Russo <jdrusso@users.noreply.github.com>
    
    * Expose pcoord-clustering parameter
    
    * Expose ray kwargs
    
    * Expose ray address parameter for plugin
    
    * More accurate documentation for optimization
    
    * Add update_cluster_structures to documentation
    
    Co-authored-by: John Russo <jdrusso@users.noreply.github.com>

--- msm_we/_hamsm/_dimensionality.py
+++ msm_we/_hamsm/_dimensionality.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import IncrementalPCA as iPCA
 import concurrent
 import multiprocessing as mp
 import numpy as np
+from sklearn.decomposition import PCA
 from deeptime.decomposition import TICA, VAMP
 from msm_we._logging import log, ProgressBar
 
@@ -10,10 +11,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from msm_we import modelWE
 
+SUPPORTED_METHODS = ["none", "pca", "vamp", "tica", "batch-pca"]
+
 
 class DimensionalityReductionMixin:
     dimReduceMethod = None
-    """str: Dimensionality reduction method. Must be one of "pca", "vamp", or "none" (**NOT** NoneType)"""
+    """str: Dimensionality reduction method. Must be one of _dimensionality.SUPPORTED_METHODS (**NOT** NoneType)"""
 
     vamp_lag = None
     vamp_dim = None
@@ -55,12 +58,7 @@ class DimensionalityReductionMixin:
         # log.debug("Reducing coordinates")
 
         # TODO: This list should not be stored here, this should be a class attribute or something
-        if (
-                self.dimReduceMethod == "none"
-                or self.dimReduceMethod == "pca"
-                or self.dimReduceMethod == "vamp"
-                or self.dimReduceMethod == "tica"
-        ):
+        if self.dimReduceMethod in SUPPORTED_METHODS:
             coords = self.processCoordinates(coords)
             coords = self.coordinates.transform(coords)
             return coords
@@ -109,15 +107,15 @@ class DimensionalityReductionMixin:
         return ipca, used_iters
 
     def dimReduce(
-            self: "modelWE",
-            first_iter=1,
-            first_rough_iter=None,
-            last_iter=None,
-            rough_stride=10,
-            fine_stride=1,
-            variance_cutoff=0.95,
-            use_weights=True,
-            progress_bar=None
+        self: "modelWE",
+        first_iter=1,
+        first_rough_iter=None,
+        last_iter=None,
+        rough_stride=10,
+        fine_stride=1,
+        variance_cutoff=0.95,
+        use_weights=True,
+        progress_bar=None,
     ):
         """
         Dimensionality reduction using the scheme specified in initialization.
@@ -138,7 +136,6 @@ class DimensionalityReductionMixin:
         """
 
         log.debug(f"Running dimensionality reduction -- method: {self.dimReduceMethod}")
-
 
         # log.debug(self.coordSet)
         if self.dimReduceMethod == "pca":
@@ -166,7 +163,9 @@ class DimensionalityReductionMixin:
                 rough_iters = range(first_rough_iter, last_iter, rough_stride)
 
             with ProgressBar(progress_bar) as progress_bar:
-                task = progress_bar.add_task(description="Initial iPCA", total=len(rough_iters))
+                task = progress_bar.add_task(
+                    description="Initial iPCA", total=len(rough_iters)
+                )
 
                 for iteration in rough_iters:
                     # TODO: Allow  chunking here so you don't have  to  go 1  by  1, but N by N
@@ -177,20 +176,23 @@ class DimensionalityReductionMixin:
                     # Additionally, 'fork' is a little faster than  spawn. Ironically, that's usually at the cost  of memory
                     #   usage. But here, the memory being used by the main thread (and therefore being copied here) isn't
                     #   that great -- the memory issue stems from it not being freed up between successive calls.
+
+                    # TODO: This, and the call below to do_full_pca, both sometimes indefinitely hang.
                     with concurrent.futures.ProcessPoolExecutor(
-                            max_workers=1, mp_context=mp.get_context("fork")
+                        max_workers=1, mp_context=mp.get_context("fork")
                     ) as executor:
                         rough_ipca = executor.submit(
-                            self.do_pca, [rough_ipca, iteration, self.processCoordinates]
+                            self.do_pca,
+                            [rough_ipca, iteration, self.processCoordinates],
                         ).result()
 
                     progress_bar.update(task, advance=1)
 
             components_for_var = (
-                    np.argmax(
-                        np.cumsum(rough_ipca.explained_variance_ratio_) > variance_cutoff
-                    )
-                    + 1
+                np.argmax(
+                    np.cumsum(rough_ipca.explained_variance_ratio_) > variance_cutoff
+                )
+                + 1
             )
             log.debug(f"Keeping {components_for_var} components")
             components_for_var = min(
@@ -203,7 +205,9 @@ class DimensionalityReductionMixin:
             extra_iters_used = 0
             iterations = range(first_iter, last_iter, fine_stride)
             with ProgressBar(progress_bar) as progress_bar:
-                task = progress_bar.add_task(total=len(iterations), completed=0, description="iPCA")
+                task = progress_bar.add_task(
+                    total=len(iterations), completed=0, description="iPCA"
+                )
 
                 for iteration in iterations:
 
@@ -221,11 +225,16 @@ class DimensionalityReductionMixin:
                     # In fact, I first moved partial_fit alone to a subprocess, but that didn't help. The issue isn't
                     #   partial_fit, it's actually loading the coords.
                     with concurrent.futures.ProcessPoolExecutor(
-                            max_workers=1, mp_context=mp.get_context("fork")
+                        max_workers=1, mp_context=mp.get_context("fork")
                     ) as executor:
                         ipca, extra_iters_used = executor.submit(
                             self.do_full_pca,
-                            [ipca, iteration, self.processCoordinates, components_for_var],
+                            [
+                                ipca,
+                                iteration,
+                                self.processCoordinates,
+                                components_for_var,
+                            ],
                         ).result()
 
                     progress_bar.advance(task, 1 + extra_iters_used)
@@ -233,30 +242,7 @@ class DimensionalityReductionMixin:
             self.coordinates = ipca
             self.ndim = components_for_var
 
-        # elif self.dimReduceMethod == "vamp":
-        #     # TODO: I  don't think trajSet is initialized by itself -- you need to manually call get_traj_coordinates
-        #     log.warning(
-        #         "VAMP dimensionality reduction requires you to *manually* call get_traj_coordinates first, "
-        #         "or self.trajSet will be all None. Make sure you've done that!"
-        #     )
-        #     raise NotImplementedError
-        #
-        #     ntraj = len(self.trajSet)
-        #     data = [None] * ntraj
-        #     for itraj in range(ntraj):
-        #         data[itraj] = self.processCoordinates(self.trajSet[itraj])
-        #     self.coordinates = coor.vamp(
-        #         data,
-        #         lag=self.vamp_lag,
-        #         dim=self.vamp_dim,
-        #         scaling=None,
-        #         right=False,
-        #         stride=1,
-        #         skip=0,
-        #     )
-        #     self.ndim = self.coordinates.dimension()
-
-        elif self.dimReduceMethod == "tica" or self.dimReduceMethod == "vamp":
+        elif self.dimReduceMethod in ["tica", "vamp", "batch-pca"]:
 
             # TODO: Streaming implementation, a la
             #  https://deeptime-ml.github.io/latest/api/generated/deeptime.decomposition.TICA.html#deeptime.decomposition.TICA.partial_fit
@@ -271,9 +257,11 @@ class DimensionalityReductionMixin:
 
             iterations = range(first_iter, last_iter, fine_stride)
             with ProgressBar(progress_bar) as progress_bar:
-                task = progress_bar.add_task(total=len(iterations),
-                                                     completed=0,
-                                                     description=f"Loading data for {self.dimReduceMethod.upper()}")
+                task = progress_bar.add_task(
+                    total=len(iterations),
+                    completed=0,
+                    description=f"Loading data for {self.dimReduceMethod.upper()}",
+                )
                 for iteration in range(first_iter, last_iter, fine_stride):
                     # iter_coords = self.get_iter_coordinates(iteration)
                     self.load_iter_data(iteration)
@@ -304,6 +292,8 @@ class DimensionalityReductionMixin:
                 self.coordinates = VAMP(
                     lagtime=1, var_cutoff=variance_cutoff, scaling="kinetic_map"
                 )
+            elif self.dimReduceMethod == "batch-pca":
+                self.coordinates = PCA(n_components=None)
 
             # self.coordinates.fit(trajs)
             log.info(f"Performing weighted {self.dimReduceMethod}")
@@ -314,17 +304,33 @@ class DimensionalityReductionMixin:
             if not use_weights or self.dimReduceMethod == "vamp":
                 weights = None
 
-            self.coordinates.fit_from_timeseries(
-                (np.array(trajs_start), np.array(trajs_end)), weights=weights
-            )
+            if self.dimReduceMethod in ["vamp", "tica"]:
+                self.coordinates.fit_from_timeseries(
+                    (np.array(trajs_start), np.array(trajs_end)), weights=weights
+                )
 
-            # Note: ndim is only used in one place, and it's a deprecated obsolete function
-            self.ndim = self.coordinates.model.output_dimension
+                # Note: ndim is only used in one place, and it's a deprecated obsolete function
+                self.ndim = self.coordinates.model.output_dimension
 
-            log.info(
-                f"Weighted {self.dimReduceMethod} will reduce "
-                f"{self.coordinates._model._instantaneous_coefficients.shape[0]} to {self.ndim} components."
-            )
+                log.info(
+                    f" {self.dimReduceMethod} will reduce "
+                    f"{self.coordinates._model._instantaneous_coefficients.shape[0]} to {self.ndim} components."
+                )
+
+            elif self.dimReduceMethod in ["batch-pca"]:
+
+                data_array = np.concatenate(
+                    [np.array(trajs_start), np.array(trajs_end)]
+                )
+                print(f"Attempting to fit PCA to array of shape {data_array.shape}")
+                self.coordinates.fit(data_array)
+
+                self.ndim = self.coordinates.n_components_
+
+                log.info(
+                    f" {self.dimReduceMethod} will reduce {self.coordinates.n_features_} "
+                    f"to {self.coordinates.n_components_} components."
+                )
 
         elif self.dimReduceMethod == "none":
             self.ndim = int(3 * self.nAtoms)
