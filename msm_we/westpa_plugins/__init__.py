69fd248fea850c5408c5ba3ac1fad89ab6ba057e
John Russo <jdrusso@users.noreply.github.com>
2022-12-02 11:49:39 -0800

    Added warning if SynD not available, instead of failing

--- msm_we/westpa_plugins/__init__.py
+++ msm_we/westpa_plugins/__init__.py
@@ -1,4 +1,16 @@
 from .augmentation_driver import MDAugmentationDriver
 from .hamsm_driver import HAMSMDriver
-from .optimization_driver import OptimizationDriver
 from .restart_driver import RestartDriver
+
+__all__ = ["MDAugmentationDriver", "HAMSMDriver", "RestartDriver"]
+
+import logging
+
+msm_we_logger = logging.getLogger("msm_we.msm_we")
+
+try:
+    from .optimization_driver import OptimizationDriver  # noqa
+except ImportError:
+    msm_we_logger.warn("Couldn't import SynD -- optimization not available")
+else:
+    __all__.append("OptimizationDriver")
